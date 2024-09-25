97fc122c23f46dff031d908e1f116ac3f8d962a4
trunglienquy <trunggaming65@gmail.com>
2024-09-25 14:09:29 +0700

    test commit in linux

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -64,7 +64,7 @@ const Menu = () => {
         </p>
         {/* Category filter */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' :'hidden'} sm:block`}>
-            <p className="mb-3 text-sm font-medium">ĐỒ UỐNG</p>
+            <p className="mb-3 text-sm font-medium">ĐỒ UỐNG - NEED CHANGE!!!</p>
             <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
                 <p className="flex gap-2">
                     <input className="w-3" type="checkbox" value={'Men'} onChange={toggleCategory} /> Men
