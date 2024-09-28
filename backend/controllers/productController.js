9ee0653fe76ee663d325e3b1e830d0f5ab24220c
trunglienquy <trunggaming65@gmail.com>
2024-09-28 09:53:16 +0700

    add product on cloundinary, mongoDB

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
-
+import { v2 as cloudinary} from 'cloudinary';
+import productModel from '../models/productModel.js';
 
 // function for add product
 const addProduct = async(req, res) => {
@@ -7,18 +8,39 @@ const addProduct = async(req, res) => {
 
         const { name, description, price, category, sizes, bestseller } = req.body;
 
-        if (!req.files || !req.files.image1 || !req.files.image2 || !req.files.image3 || !req.files.image4) {
-            return res.json({ success: false, message: 'Files not uploaded correctly' });
+        const image1 =  req.files.image1 && req.files.image1[0];
+        const image2 =  req.files.image2 && req.files.image2[0];
+        const image3 =  req.files.image3 && req.files.image3[0];
+        const image4 =  req.files.image4 && req.files.image4[0];
+
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined); // make sure to remove undefined values
+
+        // Upload images to cloudinary
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path,{resource_type: 'image'});
+                return result.secure_url; 
+            })
+        );
+        
+        const productData = {
+            name,
+            description,
+            price: Number(price),
+            category,
+            sizes: JSON.parse(sizes),
+            bestseller: bestseller === 'true' ? true : false,
+            image: imagesUrl,
+            date: Date.now(),
         }
 
-        const image1 = req.files.image1[0];
-        const image2 = req.files.image2[0];
-        const image3 = req.files.image3[0];
-        const image4 = req.files.image4[0];
+        console.log(productData)
 
-        console.log(name, description, price, category, sizes, bestseller);
-        console.log(image1, image2, image3, image4);
-        res.json({});
+        const product = new productModel(productData);
+        await product.save();
+
+        res.json({success: true, message: "Thêm sản phẩm thành công!"});
+
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: error.message });
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -6,9 +6,9 @@ const productSchema = new mongoose.Schema({
     price: { type: Number, required: true },
     image: { type: Array, required: true },
     category: { type: String, required: true },
-    size: { type: Array, required: true },
+    sizes: { type: Array, required: true },
     bestseller: { type: Boolean},
-    date: { type: Number, required: true}
+    date: { type: Number, required: true},
 });
 
 const productModel = mongoose.model.product || mongoose.model("product", productSchema)
