57c99cab9f46915dbbedd2bf0738e4ba5db5512b
trunglienquy <trunggaming65@gmail.com>
2024-09-29 14:22:17 +0700

    add admin api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -73,7 +73,19 @@ const registerUser = async(req, res) => {
 
 // Route for admin login
 const adminLogin = async(req, res) => {
-    
+    try {
+        const { email, password } = req.body
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email + password, process.env.JWT_SECRET)
+            res.json({success:true, token})
+        }
+        else{
+            res.json({success:false, message:"Tài khoản hoặc mật khẩu không đúng" })
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
+    }
 }
 
 export { loginUser, registerUser, adminLogin }
